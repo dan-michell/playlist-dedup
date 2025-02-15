730fa6ccdd83489a86fe1c9c3b2de822d83494e0
Dan Michell <dan.michell1000@gmail.com>
2025-02-15 10:38:51 +0000

    Accordion expand icon animation

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -10,18 +10,21 @@ interface Props {
 // const props = defineProps<Props>()
 
 const showPanel = ref(false)
-
-const togglePanel = () => {
-    console.log(showPanel.value)
-    showPanel.value = !showPanel.value
-}
 </script>
 
 <template>
     <div class="border-2 border-white p-0 flex">
-        <button @click="togglePanel" variant="ghost" class="transition-all hover:scale-110">
-            <Icon v-if="showPanel" icon="proicons:chevron-up" width="30" class="transition-all" />
-            <Icon v-else icon="proicons:chevron-down" width="30" class="transition-all" />
+        <button
+            @click="showPanel = !showPanel"
+            variant="ghost"
+            class="transition-all hover:scale-110"
+        >
+            <Icon
+                icon="proicons:chevron-down"
+                width="30"
+                class="transition-transform"
+                :class="{ 'transform rotate-180': showPanel }"
+            />
             <span class="sr-only">Toggle accordion</span>
         </button>
     </div>
